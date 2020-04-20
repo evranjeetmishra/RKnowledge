8829df0f57fa304bca73c1978f94cae53c61baf1
Ranjeet <ranjeet@entityvibes.com>
2020-04-20 20:07:12 +0530

    ranjeet

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello Ranjeet kumar Mishra" );
         System.out.println( "Hello Ranjeet kumar Mishra" );
         System.out.println( "Hello Ranjeet kumar Mishra" );
+        System.out.println( "Hello Ranjeet kumar Mishra" );
     }
 }

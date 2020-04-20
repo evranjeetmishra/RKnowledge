b0271b9e3ccfab4d1042b53cc0b769f9cd1920fe
evranjeetmishra <61077743+evranjeetmishra@users.noreply.github.com>
2020-04-20 19:14:28 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Ranjeet Mishra" );
+        System.out.println( "Hello Ranjeet kumar Mishra" );
     }
 }

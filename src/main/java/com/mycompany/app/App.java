71e97bd615c7c6c853288b0157611d29b11f156b
Ranjeet Mishra <ranjeetm@entityvibes.com>
2020-08-25 17:47:21 +0530

    numberadd

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,15 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Ranjeet kumar Mishra" );
-        System.out.println( "Hello Ranjeet kumar Mishra" );
-        System.out.println( "Hello Ranjeet kumar Mishra" );
-        System.out.println( "Hello Ranjeet kumar Mishra" );
-        System.out.println( "Hello Ranjeet kumar Mishra" );
-        System.out.println( "Hello Ranjeet kumar Mishra" );
-        System.out.println( "Hello Ranjeet kumar Mishra" );
-        System.out.println( "Hello Ranjeet kumar Mishra" );
-        System.out.println( "Hello Ranjeet kumar Mishra" );
-        System.out.println( "Hello Ranjeet kumar Mishra" );
+        System.out.println( "Hello Ranjeet kumar Mishra1" );
+        System.out.println( "Hello Ranjeet kumar Mishra2" );
+        System.out.println( "Hello Ranjeet kumar Mishra3" );
+        System.out.println( "Hello Ranjeet kumar Mishra4" );
+        System.out.println( "Hello Ranjeet kumar Mishra5" );
+        System.out.println( "Hello Ranjeet kumar Mishra6" );
+        System.out.println( "Hello Ranjeet kumar Mishra7" );
+        System.out.println( "Hello Ranjeet kumar Mishra8" );
+        System.out.println( "Hello Ranjeet kumar Mishra9" );
+        System.out.println( "Hello Ranjeet kumar Mishra10" );
     }
 }
